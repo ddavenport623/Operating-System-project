cd7df5bcc1dfdff18489c79be9b6cbcccf044a13
David Davenport <ddavenport623@gmail.com>
2026-09-14 13:04:10 -0500

    trying to fix a mysterious seg fault

--- dtdavenpor42_hw1/kernel.c
+++ dtdavenpor42_hw1/kernel.c
@@ -21,7 +21,7 @@ int main() {
         for(int perc = 10; perc <= 100; perc += 10){
             print_progress(19, 4, message, perc, 10); // row, column, message, percent, width
             wait_msec(500000); // Waits half a second
-            wait_msec(500000); // Waits half a second
+            // wait_msec(500000); // Waits half a second
         }
     }
     return 0;

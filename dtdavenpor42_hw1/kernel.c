9278c46dae84a61cf4fe41c6924d82b934a43c7f
David Davenport <ddavenport623@gmail.com>
2026-09-14 11:19:03 -0500

    changed message to variable

--- dtdavenpor42_hw1/kernel.c
+++ dtdavenpor42_hw1/kernel.c
@@ -13,12 +13,14 @@ int print_progress(unsigned int row,
 void boot_win();
 int strlen(const char *);
 
+char* message = "Progress: ";
+
 // finished but untested
 int main() {
     boot_win();
     while (true){
         for(int perc = 10; perc <= 100; perc += 10){
-            print_progress(19, 4, "Progress: ", perc, 10); // row, column, message, percent, width
+            print_progress(19, 4, message, perc, 10); // row, column, message, percent, width
             wait_msec(500000); // Waits half a second
         }
     }

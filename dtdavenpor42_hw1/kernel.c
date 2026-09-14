c748cfdaf5e2f850466913fc060100084ab786c2
David Davenport <ddavenport623@gmail.com>
2026-09-14 13:05:32 -0500

    added wait time before function

--- dtdavenpor42_hw1/kernel.c
+++ dtdavenpor42_hw1/kernel.c
@@ -19,9 +19,9 @@ int main() {
     boot_win();
     while (true){
         for(int perc = 10; perc <= 100; perc += 10){
+            wait_msec(500000); // Waits half a second
             print_progress(19, 4, message, perc, 10); // row, column, message, percent, width
             wait_msec(500000); // Waits half a second
-            // wait_msec(500000); // Waits half a second
         }
     }
     return 0;
